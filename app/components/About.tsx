0616c646e7ea810b14fa5a31c516e8b8a7ac0e01
rob-cost <robertocostantino3@gmail.com>
2025-07-30 16:12:35 +0200

    Updated About section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -35,7 +35,11 @@ const About = () => {
           <h4 className="my-6 text-gray-700 font-Ovo">Tools I used</h4>
           <ul className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index) => (
-              <li key={index}>
+              <li
+                className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 
+                rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
+                key={index}
+              >
                 <Image src={tool} alt="tool" className="w-5 sm:w-7" />
               </li>
             ))}
