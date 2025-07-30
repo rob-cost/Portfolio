3f85ecb1cbae03f17df81a1e72774631c9236943
rob-cost <robertocostantino3@gmail.com>
2025-07-30 16:12:24 +0200

    Adjusted navbar
    Adjusted navbar scrolling background color

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef<HTMLUListElement>(null);
 
   const openMenu = () => {
@@ -12,6 +13,16 @@ const Navbar = () => {
     if (sideMenuRef.current)
       sideMenuRef.current.style.transform = "translateX(16rem)";
   };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -23,7 +34,11 @@ const Navbar = () => {
           className="w-full"
         ></Image>
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+        }`}
+      >
         <a href="#top">
           <Image
             src="/logo.png"
@@ -34,7 +49,11 @@ const Navbar = () => {
           ></Image>
         </a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            isScroll ? "" : " bg-white shadow-sm bg-opacity-50"
+          }`}
+        >
           <li>
             <a href="#top" className="font-Ovo">
               Home
