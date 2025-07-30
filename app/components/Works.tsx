d8194ccd2a4471c6ce74bd58bdd2c25eed72685e
rob-cost <robertocostantino3@gmail.com>
2025-07-30 16:32:53 +0200

    Created a work component

---
+++ app/components/Works.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Works() {
+  return (
+    <div id="works" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">My Portfolio</h4>
+      <h2 className="text-center text-5lg font-Ovo">My latest work</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+        debitis magni commodi sint autem odit voluptates ad id, totam nisi modi,
+        placeat reiciendis? Nulla qui eligendi odio saepe nesciunt
+        necessitatibus.
+      </p>
+    </div>
+  );
+}
+
+export default Works;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Works from "./components/Works";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar></Navbar>
       <Header></Header>
       <About></About>
+      <Works></Works>
     </>
   );
 }
