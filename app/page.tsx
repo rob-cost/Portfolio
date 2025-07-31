6b23acaac2fe7a8e695220baaaf9ee3a2be8eeae
rob-cost <robertocostantino3@gmail.com>
2025-07-31 11:37:34 +0200

    Implemented style of Contact form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Works from "./components/Works";
@@ -11,6 +12,7 @@ export default function Home() {
       <Header></Header>
       <About></About>
       <Works></Works>
+      <Contact></Contact>
     </>
   );
 }
