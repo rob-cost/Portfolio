de3ecd22acefdde5cde04d30da081e24bb88a8de
rob-cost <robertocostantino3@gmail.com>
2025-07-31 15:55:39 +0200

    Implemented animation for About component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,15 +1,46 @@
 import React from "react";
 import Image from "next/image";
 import { toolsData } from "@/public/assets";
+import { motion } from "motion/react";
 
 const About = () => {
   return (
-    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
-      <h2 className="text-center text-5xl font-Ovo">About me</h2>
+    <motion.div
+      id="about"
+      className="w-full px-[12%] py-10 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h4
+        className="text-center mb-2 text-lg font-Ovo"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      >
+        Introduction
+      </motion.h4>
+      <motion.h2
+        className="text-center text-5xl font-Ovo"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        About me
+      </motion.h2>
 
-      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+      <motion.div
+        className="flex w-full flex-col lg:flex-row items-center gap-20 my-20"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+      >
+        <motion.div
+          className="w-64 sm:w-80 rounded-3xl max-w-none"
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+        >
           <Image
             src="/user-image.png"
             alt="user"
@@ -17,8 +48,13 @@ const About = () => {
             height={500}
             className="w-full rounded-3xl"
           />
-        </div>
-        <div className="flex-1">
+        </motion.div>
+        <motion.div
+          className="flex-1"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+        >
           <p className="mb-10 max-w-2xl font-Ovo">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum ex
             nesciunt porro! Odit eius obcaecati tenetur dolorem corporis
@@ -32,23 +68,34 @@ const About = () => {
                 <h3>{title}</h3>
                 <p>{description}</p> */}
           </ul>
-          <h4 className="my-6 text-gray-700 font-Ovo dark:text-white/80">
+          <motion.h4
+            className="my-6 text-gray-700 font-Ovo dark:text-white/80"
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1.3 }}
+          >
             Tools I used
-          </h4>
-          <ul className="flex items-center gap-3 sm:gap-5">
+          </motion.h4>
+          <motion.ul
+            className="flex items-center gap-3 sm:gap-5"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1 }}
+          >
             {toolsData.map((tool, index) => (
-              <li
+              <motion.li
+                whileHover={{ scale: 1.1 }}
                 className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 
                 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
                 key={index}
               >
                 <Image src={tool} alt="tool" className="w-5 sm:w-7" />
-              </li>
+              </motion.li>
             ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+          </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
