d96f73f008da8aee5e639d822c778c447824345d
rob-cost <robertocostantino3@gmail.com>
2025-07-31 11:20:23 +0200

    Created a Contact component

---
+++ app/components/Contact.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id="contact" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
+      <h2 className="text-center text-5xl font-Ovo">Get in touch!</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        If you have any questions, comments, or feeedback, please use the fomr
+        below.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
